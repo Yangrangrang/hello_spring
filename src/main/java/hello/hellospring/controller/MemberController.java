6dd099a56770525207c5fe915ef33edb7bc31b07
yangrangrang <gkssk2309@gmail.com>
2023-09-25 22:14:08 +0900

    createMember

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller // 컴포넌트 스캔, 자동의존관계 설정
 public class MemberController {
@@ -12,5 +15,20 @@ public class MemberController {
     @Autowired  // 연관관계 ( 연결 해준다)
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create (MemberForm memberForm){
+        Member member = new Member();
+        member.setName(memberForm.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
