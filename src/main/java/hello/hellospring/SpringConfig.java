ded87ef0e6d93aa92f6c24b63aff0c7e964e6ef8
yangrangrang <gkssk2309@gmail.com>
2023-10-04 20:51:25 +0900

    jdbcMemberRepository

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration  // 어노테이션을 사용하지 않고 자바에서 수동으로 bean 등록 해주는 방법
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +26,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
     // 자바코드로 직접 스프링빈을 등록하는 방법
