9eeb24123beb41a4c4ae85d82cc8d117ab4f2418
yangrangrang <gkssk2309@gmail.com>
2023-10-05 21:09:53 +0900

    springTest, transactional

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 // 단축키 command+shift+T
-@SpringBootTest //스프링 컨테이너와 테스트를 함께 실행한다.
+@SpringBootTest //스프링 컨테이너와 테스트를 함께 실행한다. // 컨테이너와 테스트를 같이 하는건 통합테스트 // MemberServiceTest 단위 테스트
 @Transactional // 테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고, 테스트 완료 후에 항상 롤백한다. 이렇게 하면 DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지 않는다.
 class MemberServiceIntegrationTest {
 
