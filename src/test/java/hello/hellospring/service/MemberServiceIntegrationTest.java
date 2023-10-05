f311e852558356e1c63f8873b2cc83d31dcc9f99
yangrangrang <gkssk2309@gmail.com>
2023-10-05 21:09:39 +0900

    springTest, transactional

---
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,57 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+// 단축키 command+shift+T
+@SpringBootTest //스프링 컨테이너와 테스트를 함께 실행한다.
+@Transactional // 테스트 케이스에 이 애노테이션이 있으면, 테스트 시작 전에 트랜잭션을 시작하고, 테스트 완료 후에 항상 롤백한다. 이렇게 하면 DB에 데이터가 남지 않으므로 다음 테스트에 영향을 주지 않는다.
+class MemberServiceIntegrationTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memoryMemberRepository;
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("spring1");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void checkMemberJoin (){
+        // given
+        Member member1 = new Member();
+        member1.setName("spring1");
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+
+        // when
+        memberService.join(member1);
+        // 예외 처리 IllegalStateException 에러가 날꺼다. 다른 Exception 넣으면 테스트 실패나옴 ex) NullPointerException
+        // 메세지도 반환이 된다. 단축키 command + option + v
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+    }
+}
